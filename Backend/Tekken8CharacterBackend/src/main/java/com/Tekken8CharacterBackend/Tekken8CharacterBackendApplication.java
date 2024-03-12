84407521f9173aa594025f9cff150f997382b57c
MalikMitchell9930 <malikmitchell9930@gmail.com>
2024-03-12 16:48:56 -0400

    second message

---
+++ Backend/Tekken8CharacterBackend/src/main/java/com/Tekken8CharacterBackend/Tekken8CharacterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Tekken8CharacterBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tekken8CharacterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tekken8CharacterBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/Tekken8CharacterBackend/src/test/java/com/Tekken8CharacterBackend/Tekken8CharacterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tekken8CharacterBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tekken8CharacterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
